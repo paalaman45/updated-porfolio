824f7189a2182aed041c22dd23516368862bb8b3
Kissel James Paalaman <kisseljamespaalam@gmail.com>
2024-11-14 08:59:45 +0800

    Header and Hero Banner

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+  import MainHeader from './components/header/MainHeader.vue';
+</script>
+
+<template>
+  <MainHeader />
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+  import HomeHero from '@/components/hero/HomeHero.vue';
+</script>
+
+<template>
+  <HomeHero />
+</template>
